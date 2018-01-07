e478706bb1ca62da64686694e4a3396302ade322
David Sanson <dsanson@gmail.com>
2018-01-07 13:15:25 -0600

    adding cask for logic 2010

---
+++ Casks/logic-2010.rb
@@ -0,0 +1,10 @@
+cask 'logic-2010' do
+  version :latest
+  sha256 :no_check 
+
+  url 'http://logiclx.humnet.ucla.edu/auto_remote/desktop/20161226/InstallLogic2010_mac_20161226.zip'
+  name 'Logic 2010'
+  homepage 'http://logiclx.humnet.ucla.edu'
+
+  app 'Logic 2010.app'
+end
