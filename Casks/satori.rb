454c629c9f903e85ffe2e80d35d181fbb0043b55
David Sanson <dsanson@gmail.com>
2024-05-18 13:05:00 -0500

    adding sha256

--- Casks/satori.rb
+++ Casks/satori.rb
@@ -1,7 +1,6 @@
 cask "satori" do
   version "2.1.2"
-  sha256 ""
-
+  sha256 "bb7cb8df61e1954e8279c816b70825fe275e14708c0b3f0138035b9e80bec021"
   url "http://downloads.sticksoftware.com/Satori.dmg"
   name "Satori screensaver"
   desc "A meditative screensaver"
