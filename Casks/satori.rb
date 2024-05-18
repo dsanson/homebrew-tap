d7241662a0f8c2bcc35baa3c4d0248c59f34271f
David Sanson <dsanson@gmail.com>
2024-05-18 12:58:34 -0500

    adding satori cask

---
+++ Casks/satori.rb
@@ -0,0 +1,13 @@
+cask "satori" do
+  version "2.1.2"
+  sha256 ""
+
+  url "http://downloads.sticksoftware.com/Satori.dmg"
+  name "Satori screensaver"
+  desc "A meditative screensaver"
+  homepage "http://www.sticksoftware.com/software/Satori.html"
+
+  screen_saver "Satori.saver"
+
+  # No zap stanza required
+end
